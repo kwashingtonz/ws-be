e76e8d359ce56fd59cb5785f3fb10a4c9f78d71b
Kalindu Washington <kalinduwashington@gmail.com>
2024-03-14 23:27:22 +0530

    added reading route fiel

--- src/configuration/route-configuration.ts
+++ src/configuration/route-configuration.ts
@@ -1,5 +1,6 @@
 var systemRoutes = require("../routes/system-routes");
 var setupRoutes = require("../routes/setup-routes");
+var readingRoutes = require("../routes/reading-routes");
 
 import express, { Request, Response, Router } from "express";
 
@@ -7,5 +8,6 @@ export class RouteConfiguration {
   configRoutes(app: express.Application) {
     app.use("/service/system", systemRoutes);
     app.use("/service/setup", setupRoutes);
+    app.use("/service/reading", readingRoutes);
   }
 }
